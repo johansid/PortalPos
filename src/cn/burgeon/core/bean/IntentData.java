b26afce882b66316435533931d5060ea8989cb3a
zhanghuan0723 <simon0723@126.com>
2014-04-25 09:02:59 +0800

    传递对象方式改为实现Serializable

--- src/cn/burgeon/core/bean/IntentData.java
+++ src/cn/burgeon/core/bean/IntentData.java
@@ -1,13 +1,12 @@
 package cn.burgeon.core.bean;
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import java.io.Serializable;
 
 /**
  * Created by Simon on 2014/4/22.
  * 页面传递数据
  */
-public class IntentData implements Parcelable {
+public class IntentData implements Serializable {
     private String store;
     private String user;
 
@@ -27,32 +26,4 @@ public class IntentData implements Parcelable {
         this.user = user;
     }
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(this.store);
-        dest.writeString(this.user);
-    }
-
-    public IntentData() {
-    }
-
-    private IntentData(Parcel in) {
-        this.store = in.readString();
-        this.user = in.readString();
-    }
-
-    public static Parcelable.Creator<IntentData> CREATOR = new Parcelable.Creator<IntentData>() {
-        public IntentData createFromParcel(Parcel source) {
-            return new IntentData(source);
-        }
-
-        public IntentData[] newArray(int size) {
-            return new IntentData[size];
-        }
-    };
 }
