84127e528ffe91c4ce69cb20bedd566debaecf02
zhanghuan0723 <simon0723@126.com>
2014-04-25 11:43:31 +0800

    调拨出库数据化

---
+++ src/cn/burgeon/core/adapter/AllotOutLVAdapter.java
@@ -0,0 +1,78 @@
+package cn.burgeon.core.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import cn.burgeon.core.R;
+import cn.burgeon.core.bean.AllotOut;
+
+/**
+ * Created by Simon on 2014/4/16.
+ */
+public class AllotOutLVAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private ArrayList<AllotOut> list;
+    private int mLayoutRes;
+
+    public AllotOutLVAdapter(Context c, ArrayList<AllotOut> l, int layoutRes) {
+        this.mContext = c;
+        this.list = l;
+        this.mLayoutRes = layoutRes;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(mLayoutRes, null);
+            holder = new ViewHolder();
+            holder.docnoTV = (TextView) convertView.findViewById(R.id.docnoTV);
+            holder.uploadStateTV = (TextView) convertView.findViewById(R.id.uploadStateTV);
+            holder.docStateIV = (TextView) convertView.findViewById(R.id.docStateIV);
+            holder.billdateIV = (TextView) convertView.findViewById(R.id.billdateIV);
+            holder.cdestidIV = (TextView) convertView.findViewById(R.id.cdestidIV);
+            holder.totqtyoutIV = (TextView) convertView.findViewById(R.id.totqtyoutIV);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        // 赋值
+        holder.docnoTV.setText(list.get(position).getDOCNO());
+        holder.billdateIV.setText(list.get(position).getBILLDATE());
+        holder.cdestidIV.setText(list.get(position).getC_DEST_ID());
+        holder.totqtyoutIV.setText(list.get(position).getTOT_QTYOUT());
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView docnoTV;
+        TextView uploadStateTV;
+        TextView docStateIV;
+        TextView billdateIV;
+        TextView cdestidIV;
+        TextView totqtyoutIV;
+    }
+}
--- src/cn/burgeon/core/ui/allot/AllotOutActivity.java
+++ src/cn/burgeon/core/ui/allot/AllotOutActivity.java
@@ -2,13 +2,37 @@ package cn.burgeon.core.ui.allot;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.android.volley.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import cn.burgeon.core.App;
 import cn.burgeon.core.R;
+import cn.burgeon.core.adapter.AllotInLVAdapter;
+import cn.burgeon.core.adapter.AllotOutLVAdapter;
+import cn.burgeon.core.bean.AllotIn;
+import cn.burgeon.core.bean.AllotOut;
 import cn.burgeon.core.ui.BaseActivity;
+import cn.burgeon.core.utils.PreferenceUtils;
+import cn.burgeon.core.utils.ScreenUtils;
 
 public class AllotOutActivity extends BaseActivity {
-    private Button addBtn, queryBtn, delBtn, backBtn;
+    private ListView allotOutLV;
+    private TextView recodeNumTV;
+    private Button addBtn, queryBtn, delBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +41,87 @@ public class AllotOutActivity extends BaseActivity {
         setContentView(R.layout.activity_allot_out);
 
         init();
+
+        initLVData();
     }
 
     private void init() {
+        // 初始化门店信息
+        TextView storeTV = (TextView) findViewById(R.id.storeTV);
+        storeTV.setText(App.getPreferenceUtils().getPreferenceStr(PreferenceUtils.store_key));
+
+        TextView currTimeTV = (TextView) findViewById(R.id.currTimeTV);
+        currTimeTV.setText(getCurrDate());
+
+        HorizontalScrollView hsv = (HorizontalScrollView) findViewById(R.id.hsv);
+        ViewGroup.LayoutParams params = hsv.getLayoutParams();
+        params.height = (int) ScreenUtils.getAllotInLVHeight(this);
+
+        allotOutLV = (ListView) findViewById(R.id.allotoutLV);
+        recodeNumTV = (TextView) findViewById(R.id.recodeNumTV);
+
         addBtn = (Button) findViewById(R.id.addBtn);
         addBtn.setOnClickListener(new ClickEvent());
         queryBtn = (Button) findViewById(R.id.queryBtn);
         delBtn = (Button) findViewById(R.id.delBtn);
-        backBtn = (Button) findViewById(R.id.backBtn);
+    }
+
+    private void initLVData() {
+        Map<String, String> params = new HashMap<String, String>();
+        try {
+            JSONArray array = new JSONArray();
+            JSONObject transactions = new JSONObject();
+            transactions.put("id", 112);
+            transactions.put("command", "Query");
+
+            JSONObject paramsInTransactions = new JSONObject();
+            paramsInTransactions.put("table", "M_TRANSFER");
+            paramsInTransactions.put("columns", new JSONArray().put("DOCNO").put("BILLDATE").put("C_DEST_ID").put("TOT_QTYOUT"));
+            transactions.put("params", paramsInTransactions);
+
+            array.put(transactions);
+            params.put("transactions", array.toString());
+            sendRequest(params, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    // 取消进度条
+                    stopProgressDialog();
+
+                    try {
+                        ArrayList<AllotOut> lists = resJAToList(response);
+                        AllotOutLVAdapter mAdapter = new AllotOutLVAdapter(AllotOutActivity.this, lists, R.layout.allot_out_item);
+                        allotOutLV.setAdapter(mAdapter);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private ArrayList<AllotOut> resJAToList(String response) throws JSONException {
+        ArrayList<AllotOut> lists = new ArrayList<AllotOut>();
+
+        JSONArray resJA = new JSONArray(response);
+        JSONObject resJO = resJA.getJSONObject(0);
+        JSONArray rowsJA = resJO.getJSONArray("rows");
+        int len = rowsJA.length();
+        recodeNumTV.setText(len + "条记录");
+        for (int i = 0; i < len; i++) {
+            // ["TF0912140000005",20091214,3909,11]
+            String currRow = rowsJA.get(i).toString();
+            String[] currRows = currRow.split(",");
+
+            AllotOut allotOut = new AllotOut();
+            allotOut.setDOCNO(currRows[0].substring(2, currRows[0].length() - 1));
+            allotOut.setBILLDATE(currRows[1]);
+            allotOut.setC_DEST_ID(currRows[2]);
+            allotOut.setTOT_QTYOUT(currRows[3].substring(0, currRows[3].length() - 1));
+            lists.add(allotOut);
+        }
+        return lists;
     }
 
     class ClickEvent implements View.OnClickListener {
